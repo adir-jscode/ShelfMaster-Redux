6bfb8b36154befc3e515f77d27414855af1d0489
adir-jscode <adir.jscode@gmail.com>
2025-07-04 10:21:35 +0600

    redux installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/layout/Navbar";
+import "./App.css";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router";
+
 export default function Navbar() {
   return (
-    <nav className="w-full bg-background border-b">
-      <h1>hello</h1>
+    <nav className="max-2-7xl max-auto h-16 flex justify-between items-center gap-3 px-5">
+      <div className="flex items-center">
+        <span className="font-bold ml-2">ShelfMaster</span>
+      </div>
+      <Link to={"/books"}>Books</Link>
+      <Link to={"/books"}>Add Book</Link>
+      <Link to={"/books"}>Borrow Summary</Link>
     </nav>
   );
 }
