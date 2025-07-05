d3832173f8b2e2a5ddbb141d5e7787571b204a1f
adir-jscode <adir.jscode@gmail.com>
2025-07-05 17:02:45 +0600

    updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${process.env.VITE_API_URL}` }),
   tagTypes: ["books", "borrow"],
   endpoints: (builder) => ({
     getBooks: builder.query({
